49eef9077b3416dae3273ac5b4acbd42b61e0204
Athulsn10 <athulnair10@outlook.com>
2023-11-25 01:57:25 +0530

    login,sigup authentication

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,45 +1,90 @@
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
-  const submitHandler = () =>{
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-  }
+
+  const submitHandler = async() =>{
+    setLoading(true);
+    if(!email || !password){
+      toast.warn("Fill all fields")
+      setLoading(false)
+    }
+    try {
+      const config = {
+        headers: {
+          "content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        "/api/user/login",
+        { email, password},
+        config
+      );
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      navigate("/chats");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
     <div className="my-2">
         <div className=" d-flex align-items-center justify-content-center">
           <div className='container-fluid'>
             <Form.Control
-            style={{backgroundColor:'white',border:'none', color:'white'}}
+            style={{backgroundColor:'white',border:'none', color:'black'}}
             className='my-3 w-100'
               placeholder="Email"
+              value={email}
               type="email"
               onChange={(e) => setEmail(e.target.value)}
             />
             <Form.Control
-            style={{backgroundColor:'white',border:'none',color:'white'}}
+            style={{backgroundColor:'white',border:'none',color:'black'}}
             className='my-3 w-100'
               placeholder="Password"
               type="password"
+              value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
         </div>
         <div className="mb-2 d-flex align-items-center justify-content-center">
-          <button onClick={submitHandler} style={{backgroundColor:'#3c46ff',color:'white'}} className="mx-2 btn  w-100">Log In</button>
+          <button onClick={submitHandler} 
+          disabled={loading}
+          style={{backgroundColor:'#3c46ff',color:'white'}} className="mx-2 btn  w-100">Log In</button>
         </div>
         <div className="mb-2 d-flex align-items-center justify-content-center">
           <button onClick={()=>{
-            setEmail("guest@gmail.com");
+            setEmail("guestuser@gmail.com");
             setPassword("123456")
           }} 
           style={{backgroundColor:'transparent',color:'white',border:'none'}}
           className="mx-2 text-light w-100">Chat As A Guest</button>
         </div>
       </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </>
   )
 }
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function Signup() {
    const [name, setName] = useState();
@@ -7,57 +11,141 @@ function Signup() {
    const [password, setPassword] = useState();
    const [confirmpassword, setConfirmpassword] = useState();
    const [pic, setPic] = useState();
+   const [loading, setLoading] = useState(true);
+   const navigate = useNavigate();
 
-   const postDetails =(pics)=>{
+   const postDetails = (pics) => {
+     setLoading(true);
 
-   }
+     if (pics === undefined) {
+       toast.error("select your profile photo");
+       return;
+     }
+     if (
+       pics.type === "image/jpeg" ||
+       pics.type === "image/png" ||
+       pics.type === "image/jpg"
+     ) {
+       const data = new FormData();
+       data.append("file", pics);
+       data.append("upload_preset", "chatify");
+       data.append("cloud_name", "athulcloud");
+       fetch("https://api.cloudinary.com/v1_1/athulcloud/image/upload", {
+         method: "post",
+         body: data,
+       })
+         .then((res) => res.json())
+         .then((data) => {
+           setPic(data.url.toString());
+           // console.log(data.url.toString());
+           setLoading(false);
+         })
+         .catch((err) => {
+           console.log(err);
+           setLoading(false);
+         });
+     } else {
+       toast.error("select valid image");
+       setLoading(false);
+     }
+   };
 
-   const submitHandler = () =>{
-
-   }
+   const submitHandler = async () => {
+     setLoading(true);
+     if (!name || !email || !password || !confirmpassword) {
+       toast.warn("Fill all fields");
+       setLoading(false);
+       return;
+     }
+     if (password !== confirmpassword) {
+       toast.warn("Password does not match");
+       return;
+     }
+     try {
+       const config = {
+         headers: {
+           "content-type": "application/json",
+         },
+       };
+       const { data } = await axios.post(
+         "/api/user",
+         { name, email, password, pic },
+         config
+       );
+       localStorage.setItem("userInfo", JSON.stringify(data));
+       setLoading(false);
+       navigate("/chats");
+     } catch (error) {
+       console.log(error);
+     }
+   };
   return (
     <>
       <div className="my-4">
         <div className=" d-flex align-items-center justify-content-center">
-          <div className='container-fluid'>
+          <div className="container-fluid">
             <Form.Control
-            className='my-3'
+              className="my-3"
               placeholder="Username"
               type="text"
               onChange={(e) => setName(e.target.value)}
             />
             <Form.Control
-            className='my-3'
+              className="my-3"
               placeholder="Email"
               type="email"
               onChange={(e) => setEmail(e.target.value)}
             />
             <Form.Control
-            className='my-3'
+              className="my-3"
               placeholder="Password"
               type="password"
               onChange={(e) => setPassword(e.target.value)}
             />
             <Form.Control
-            className='my-3'
+              className="my-3"
               placeholder="Confirm Password"
               type="password"
               onChange={(e) => setConfirmpassword(e.target.value)}
             />
 
             <Form.Control
-            className='mt-3'
+              className="mt-3"
               type="file"
               accept="image/*"
               onChange={(e) => postDetails(e.target.files[0])}
             />
-            <p className='text-end pe-3 pt-1' style={{fontSize:'12px',color:'white'}}>Upload Your Profile Picture</p>
+            <p
+              className="text-end pe-3 pt-1"
+              style={{ fontSize: "12px", color: "white" }}
+            >
+              Upload Your Profile Picture
+            </p>
           </div>
         </div>
         <div className="mb-2 d-flex align-items-center justify-content-center">
-          <button onClick={submitHandler} style={{backgroundColor:'#3c46ff',color:'white'}} className="mx-2 btn  w-100"> Sign Up</button>
+          <button
+            onClick={submitHandler}
+            style={{ backgroundColor: "#3c46ff", color: "white" }}
+            className="mx-2 btn  w-100"
+            loading={loading}
+          >
+            Sign Up
+          </button>
         </div>
       </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </>
   );
 }
